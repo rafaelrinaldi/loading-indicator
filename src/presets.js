1ce6be24456bedbe97ce60397893cee233a1165c
Rafael Rinaldi <rafael@rinaldi.io>
2015-08-04 22:44:53 -0300

    Removed "squares" preset.

--- src/presets.js
+++ src/presets.js
@@ -2,7 +2,6 @@
 
 module.exports = {
   'sticks': '|/-\\',
-  'square': '◣◤◥',
   'circle': '◐◓◑◒',
   'dots': ['.', '..', '...'],
   'bullets': [
@@ -18,5 +17,18 @@ module.exports = {
     '▸▸▸▹▹',
     '▸▸▸▸▹',
     '▸▸▸▸▸'
+  ],
+  'clock': [
+    '\uD83D\uDD50',
+    '\uD83D\uDD51',
+    '\uD83D\uDD52',
+    '\uD83D\uDD53',
+    '\uD83D\uDD54',
+    '\uD83D\uDD55',
+    '\uD83D\uDD56',
+    '\uD83D\uDD57',
+    '\uD83D\uDD58',
+    '\uD83D\uDD59',
+    '\uD83D\uDD5A'
   ]
 };
