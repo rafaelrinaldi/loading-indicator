f8df4dc09a94068154c9c49139fa7b688daee884
Marcos Cáceres <marcos+github@marcosc.com>
2017-10-27 16:31:43 +1100

    style(presents): exclude emoji as comments

--- presets.js
+++ presets.js
@@ -30,21 +30,21 @@ module.exports = {
     '\uD83D\uDD5A'
   ],
   daynight: [
-    '\uD83C\uDF15', // 🌕
-    '\uD83C\uDF16', // 🌖
-    '\uD83C\uDF17', // 🌗
-    '\uD83C\uDF18', // 🌘
-    '\uD83C\uDF11', // 🌑
-    '\uD83C\uDF1A', // 🌚
-    '\uD83C\uDF1A', // 🌚
-    '\uD83C\uDF1A', // 🌚
-    '\uD83C\uDF1A', // 🌚
-    '\uD83C\uDF12', // 🌒
-    '\uD83C\uDF13', // 🌓
-    '\uD83C\uDF14', // 🌔
-    '\uD83C\uDF1D', // 🌝
-    '\uD83C\uDF1D', // 🌝
-    '\uD83C\uDF1D', // 🌝
-    '\uD83C\uDF1D'  // 🌝
+    '\uD83C\uDF15',
+    '\uD83C\uDF16',
+    '\uD83C\uDF17',
+    '\uD83C\uDF18',
+    '\uD83C\uDF11',
+    '\uD83C\uDF1A',
+    '\uD83C\uDF1A',
+    '\uD83C\uDF1A',
+    '\uD83C\uDF1A',
+    '\uD83C\uDF12',
+    '\uD83C\uDF13',
+    '\uD83C\uDF14',
+    '\uD83C\uDF1D',
+    '\uD83C\uDF1D',
+    '\uD83C\uDF1D',
+    '\uD83C\uDF1D'
   ]
 };
